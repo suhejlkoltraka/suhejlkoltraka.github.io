5c537cb120263ca5036e59a549d8c244e2855ecb
suhejlkoltraka <suhejl.koltraka@cit.edu.al>
2026-05-31 19:46:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+window.addEventListener("scroll", () => {
 
+const navbar = document.querySelector(".custom-navbar");
+
+if(window.scrollY > 50){
+    navbar.style.background = "rgba(0,0,0,.9)";
+}else{
+    navbar.style.background = "rgba(0,0,0,.6)";
+}
+
+});
